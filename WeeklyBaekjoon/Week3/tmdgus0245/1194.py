1e21dfc9bb43c95a850b72a139a7e0b02318653f
승현 <tmdgusdl0245@naver.com>
2024-07-22 02:13:16 +0900

    solve 1194

--- WeeklyBaekjoon/Week3/tmdgus0245/1194.py
+++ WeeklyBaekjoon/Week3/tmdgus0245/1194.py
@@ -1,5 +1,5 @@
 import sys
-from collections import deque
+
 N,M=map(int,sys.stdin.readline().split())
 maze = [0]*N
 for i in range(N):
@@ -7,72 +7,90 @@ for i in range(N):
 for y in range(N):
     for x in range(M):
         if maze[y][x]=='0':
-            queue = deque([(y,x,0,0,0,0)])
+            queue = [(y,x,0,0)]
+visited = [[[0 for _ in range(64)]for _ in range(M)]for _ in range(N)]
 while queue:
-    position = queue.popleft()
+    position = queue.pop(0)
     y = position[0]
     x = position[1]
     count = position[2]
     key = position[3]
-    pre = position[4]
-    visited = position[5]
-    if y>0 and not((pre >> 2) & 1):
+    if y>0:
         if maze[y-1][x] == '.' or maze[y-1][x] == '0':
-            queue.append((y-1,x,count+1,key,1,visited))
+            if not(visited[y-1][x][key]):
+                queue.append((y-1,x,count+1,key))
+                visited[y-1][x][key]=1
         elif ord('a') <= ord(maze[y-1][x]) <= ord('f'):
-            if (key >> (ord(maze[y-1][x]) - ord('a'))) & 1:
-                queue.append((y-1,x,count+1,key,1,visited))
-            else:
-                queue.append((y-1,x,count+1,key | int(2**(ord(maze[y-1][x])-ord('a'))),0,0))
+            if not(visited[y-1][x][key]):
+                visited[y-1][x][key]=1
+                if (key >> (ord(maze[y-1][x]) - ord('a'))) & 1:
+                    queue.append((y-1,x,count+1,key))
+                else:
+                    queue.append((y-1,x,count+1,key | int(2**(ord(maze[y-1][x])-ord('a')))))
         elif ord('A') <= ord(maze[y-1][x]) <= ord('F'):
-            if not((visited >> (ord(maze[y-1][x]) - ord('A'))) & 1):
+            if not(visited[y-1][x][key]):
                 if (key >> (ord(maze[y-1][x]) - ord('A'))) & 1:
-                    queue.append((y-1,x,count+1,key,1,visited | int(2**(ord(maze[y-1][x])-ord('A')))))
+                    visited[y-1][x][key]=1
+                    queue.append((y-1,x,count+1,key))
         elif maze[y-1][x] == '1':
             print(count+1)
             exit()
-    if x>0 and not((pre >> 3) & 1):
+    if x>0:
         if maze[y][x-1] == '.' or maze[y][x-1] == '0':
-            queue.append((y,x-1,count+1,key,2,visited))
+            if not(visited[y][x-1][key]):
+                visited[y][x-1][key]=1
+                queue.append((y,x-1,count+1,key))
         elif ord('a') <= ord(maze[y][x-1]) <= ord('f'):
-            if (key >> (ord(maze[y][x-1]) - ord('a'))) & 1:
-                queue.append((y,x-1,count+1,key,2,visited))
-            else:
-                queue.append((y,x-1,count+1,key | int(2**(ord(maze[y][x-1])-ord('a'))),0,0))
+            if not(visited[y][x-1][key]):
+                visited[y][x-1][key]=1
+                if (key >> (ord(maze[y][x-1]) - ord('a'))) & 1:
+                    queue.append((y,x-1,count+1,key))
+                else:
+                    queue.append((y,x-1,count+1,key | int(2**(ord(maze[y][x-1])-ord('a')))))
         elif ord('A') <= ord(maze[y][x-1]) <= ord('F'):
-            if not((visited >> (ord(maze[y][x-1]) - ord('A'))) & 1):
+            if not(visited[y][x-1][key]):
                 if (key >> (ord(maze[y][x-1]) - ord('A'))) & 1:
-                    queue.append((y,x-1,count+1,key,2,visited | int(2**(ord(maze[y][x-1])-ord('A')))))
+                    visited[y][x-1][key]=1
+                    queue.append((y,x-1,count+1,key))
         elif maze[y][x-1] == '1':
             print(count+1)
             exit()
-    if y<N-1 and not(pre & 1):
+    if y<N-1:
         if maze[y+1][x] == '.' or maze[y+1][x] == '0':
-            queue.append((y+1,x,count+1,key,4,visited))
+            if not(visited[y+1][x][key]):
+                visited[y+1][x][key]=1
+                queue.append((y+1,x,count+1,key))
         elif ord('a') <= ord(maze[y+1][x]) <= ord('f'):
-            if (key >> (ord(maze[y+1][x]) - ord('a'))) & 1:
-                queue.append((y+1,x,count+1,key,4,visited))
-            else:
-                queue.append((y+1,x,count+1,key | int(2**(ord(maze[y+1][x])-ord('a'))),0,0))
+            if not(visited[y+1][x][key]):
+                visited[y+1][x][key]=1
+                if (key >> (ord(maze[y+1][x]) - ord('a'))) & 1:
+                    queue.append((y+1,x,count+1,key))
+                else:
+                    queue.append((y+1,x,count+1,key | int(2**(ord(maze[y+1][x])-ord('a')))))
         elif ord('A') <= ord(maze[y+1][x]) <= ord('F'):
-            if not((visited >> (ord(maze[y+1][x]) - ord('A'))) & 1):
+            if not(visited[y+1][x][key]):
                 if (key >> (ord(maze[y+1][x]) - ord('A'))) & 1:
-                    queue.append((y+1,x,count+1,key,4,visited | int(2**(ord(maze[y+1][x])-ord('A')))))
+                    visited[y+1][x][key]=1
+                    queue.append((y+1,x,count+1,key))
         elif maze[y+1][x] == '1':
             print(count+1)
             exit()
-    if x<M-1 and not((pre >> 1) & 1):
+    if x<M-1:
         if maze[y][x+1] == '.' or maze[y][x+1] == '0':
-            queue.append((y,x+1,count+1,key,8,visited))
+            if not(visited[y][x+1][key]):
+                visited[y][x+1][key]=1
+                queue.append((y,x+1,count+1,key))
         elif ord('a') <= ord(maze[y][x+1]) <= ord('f'):
-            if (key >> (ord(maze[y][x+1]) - ord('a'))) & 1:
-                queue.append((y,x+1,count+1,key,8,visited))
-            else:
-                queue.append((y,x+1,count+1,key | int(2**(ord(maze[y][x+1])-ord('a'))),0,0))
+            if not(visited[y][x+1][key]):
+                visited[y][x+1][key]=1
+                if (key >> (ord(maze[y][x+1]) - ord('a'))) & 1:
+                    queue.append((y,x+1,count+1,key))
+                else:
+                    queue.append((y,x+1,count+1,key | int(2**(ord(maze[y][x+1])-ord('a')))))
         elif ord('A') <= ord(maze[y][x+1]) <= ord('F'):
-            if not((visited >> (ord(maze[y][x+1]) - ord('A'))) & 1):
+            if not(visited[y][x+1][key]):
                 if (key >> (ord(maze[y][x+1]) - ord('A'))) & 1:
-                    queue.append((y,x+1,count+1,key,8,visited | int(2**(ord(maze[y][x+1])-ord('A')))))
+                    queue.append((y,x+1,count+1,key))
         elif maze[y][x+1] == '1':
             print(count+1)
             exit()
