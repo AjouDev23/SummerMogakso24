d18172f597ff534cea86b9160d45c85a05a47c56
승현 <tmdgusdl0245@naver.com>
2024-07-24 18:23:18 +0900

    solve assignment problem

---
+++ WeeklyBaekjoon/Week4/tmdgus0245/13904.py
@@ -0,0 +1,16 @@
+import sys
+N = int(sys.stdin.readline().rstrip())
+assignments = [0 for _ in range(N)]
+grades=0
+for i in range(N):
+    d, w = map(int,sys.stdin.readline().rstrip().split())
+    assignments[i] = (d,w)
+assignments.sort(key = lambda x:-x[1])
+print(max(assignments,key = lambda x: x[0])[0])
+for day in range(max(assignments,key = lambda x: x[0])[0],0,-1):
+    for assignment in assignments:
+        if assignment[0]>=day:
+            grades += assignment[1]
+            assignments.remove(assignment)
+            break
+print(grades)
